d721ab413efd15cbb283b1db1c6126eafb2edf54
George <bulakh.96@gmail.com>
2023-05-01 19:59:30 +0300

    Add 'thendo'

--- src/promise.ts
+++ src/promise.ts
@@ -9,3 +9,7 @@ type AwaitFields<T> = {
 }
 export const parallelObj = async<T extends Record<string, Promise<unknown>>>(obj: T) =>
    Object.fromEntries(await Promise.all(Object.entries(obj).map(e => e[1].then(v => [e[0], v])))) as AwaitFields<T>
+
+// Can't name this `then` because it causes conflict with Svelte or Ionic
+export const thendo = <T extends unknown[], R>(f: (...args: T) => R ) => (...t: { -readonly [P in keyof T]: Promise<T[P]>; }) =>
+   Promise.all(t).then(async (t): Promise<Awaited<R>> => { return await f(...t) })
