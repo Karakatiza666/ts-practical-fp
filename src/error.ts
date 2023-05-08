913d6f8a89f20c070d23ed73c8ef56a5f149659a
George <bulakh.96@gmail.com>
2023-05-08 23:51:20 +0300

    Add assertThat

--- src/error.ts
+++ src/error.ts
@@ -1,1 +1,8 @@
 export const errorMessage = (e: any) => e.message ?? e.info ?? e.error ?? (typeof e === 'string' ? e : JSON.stringify(e))
+
+export const assertThat = <T>(f: (v: T) => boolean) => (v: T) => {
+   if (!f(v)) {
+      throw new Error(`assertThat: ${f.name} false`);
+   }
+   return v
+}
