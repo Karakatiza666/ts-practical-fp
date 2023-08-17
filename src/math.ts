48991fe8e55eeb2d6ffe6ed25ea646f6fb56654b
George <bulakh.96@gmail.com>
2023-08-17 11:16:39 +0300

    Add discreteDerivative, add yarn compatibility

--- src/math.ts
+++ src/math.ts
@@ -25,3 +25,28 @@ export function toFraction(_decimal: number) {
       }
    }
 }
+
+/**
+ * Clamp a number
+ * @param number 
+ * @param min inclusive minimum
+ * @param max inclusive maximum
+ * @returns 
+ */
+export function clamp(number: number, min: number, max: number) {
+   return Math.max(min, Math.min(number, max))
+}
+
+export const discreteDerivative = <T, R>(arr: T[], f: (n1: T, n0: T) => R) => {
+   // return arr.slice(1).map((x, i) => f(x, arr[i]))
+   if (!arr.length) {
+      return []
+   }
+   const
+      len = arr.length - 1,
+      result = new Array<R>(len)
+   for (let i = 0; i < len; ++i) {
+      result[i] = f(arr[i + 1], arr[i])
+   }
+   return result
+}
